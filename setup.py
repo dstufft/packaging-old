2f0af46c1d662aa30ac5b7d2400b7bf3de69efa8
Donald Stufft <donald.stufft@gmail.com>
2012-09-13 08:42:40 -0400

    Add pep8 testing to py.test

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[],
 
     extras_require={
-        "test": ["pytest", "pytest-cov"],
+        "test": ["pytest", "pytest-cov", "pytest-pep8"],
     },
 
     packages=find_packages(exclude=["tests"]),
