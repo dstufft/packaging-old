b5c9b239b7bd47bb04b828f7f1d973787432565a
Donald Stufft <donald.stufft@gmail.com>
2012-09-13 09:38:14 -0400

    Simplfy the predicate code

--- packaging/version.py
+++ packaging/version.py
@@ -322,7 +322,7 @@ def suggest_normalized_version(s):
 
 # A predicate is: "ProjectName (VERSION1, VERSION2, ..)
 _PREDICATE = re.compile(r"(?i)^\s*(\w[\s\w-]*(?:\.\w*)*)(.*)")
-_VERSIONS = re.compile(r"^\s*\((?P<versions>.*)\)\s*$|^\s*(?P<versions2>.*)\s*$")
+_VERSIONS = re.compile(r"^\s*\((?P<versions>.*)\)\s*$")
 _PLAIN_VERSIONS = re.compile(r"^\s*(.*)\s*$")
 _SPLIT_CMP = re.compile(r"^\s*(<=|>=|<|>|!=|==)\s*([^\s,]+)\s*$")
 
@@ -363,18 +363,10 @@ class VersionPredicate(object):
             return
 
         predicates = _VERSIONS.match(predicates.strip())
-
-        if not predicates:
-            return  # @@@ Not Covered
-
         predicates = predicates.groupdict()
-        if predicates['versions']:
-            versions = predicates['versions']
-        else:
-            versions = predicates.get('versions2')
 
-        if versions:
-            for version in versions.split(','):
+        if predicates["versions"]:
+            for version in predicates["versions"].split(","):
                 if version.strip():
                     self.predicates.append(_split_predicate(version))
 
